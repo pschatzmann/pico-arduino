6758fcbd6258852617bc9a6bc6553bdc5a2181c9
Phil Schatzmann <phil_schatzmann@yahoo.com>
2021-03-28 22:37:42 +0200

    cleanup

--- sandbox/drone/MotorsSpeedControl.cpp
+++ sandbox/drone/MotorsSpeedControl.cpp
@@ -6,25 +6,32 @@
 
 #ifdef PICO
 
+int motor_pins[] = {4,5,6,7};
 int pwm_frequency = 50000;
-PicoPWM pwm(pwm_frequency, MAX_POWER);
+MotorsSpeedControl ref;
+PicoPWM pwm(pwm_frequency, ref.GetMotorsMaxPower());
 
 void MotorsSpeedControl::Init() {
     // Set motors pin PD4, PD5, PD6, PD7 as outputs
-    for (int id=4; id<=7;id++){
-        pwm.begin(id);
+    for (int id=0; id<4;id++){
+        pwm.begin(motor_pins[id]);
     }
 }
 
 void MotorsSpeedControl::Idle() {
-    for (int id = 4; id <= 7; id++){
-        pwm.write(id, MIN_POWER);
+    for (int id=0; id<4;id++){
+        pwm.write(motor_pins[id], MIN_POWER);
     }
 }
 
-void MotorsSpeedControl::UpdateSpeed(int _id, float _PWM) {
-    Logger.printf(PicoLogger::Debug, "UpdateSpeed: %d, %f \n", _id, _PWM);
-    pwm.write(_id, _PWM);
+void MotorsSpeedControl::UpdateSpeed(int id, float pulse_width) {
+    if (pulse_width>MAX_POWER){
+        pulse_width = MAX_POWER;
+    } else if (pulse_width<MIN_POWER){
+        pulse_width = MIN_POWER;
+    }
+    Logger.printf(PicoLogger::Debug, "UpdateSpeed: %d, %f \n", id, pulse_width);
+    pwm.write(motor_pins[id], pulse_width);
 }
 
 void MotorsSpeedControl::ApplySpeed(volatile uint16_t *TCNTn, volatile uint16_t *OCRnA) {
