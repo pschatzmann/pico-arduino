edc9613c6c1f0f9632c58a496daa9fc53e4d0787
Phil Schatzmann <phil.schatzmann@gmail.com>
2021-03-31 00:27:50 +0200

    Update tf_hallo_world.cpp

--- examples/tensorflow_micro/tf_hallo_world.cpp
+++ examples/tensorflow_micro/tf_hallo_world.cpp
@@ -1,4 +1,16 @@
 #include "Arduino.h"
+#include "tensorflow/lite/micro/all_ops_resolver.h"
+#include "tensorflow/lite/micro/micro_error_reporter.h"
+#include "tensorflow/lite/micro/micro_interpreter.h"
+#include "tensorflow/lite/micro/testing/micro_test.h"
+#include "tensorflow/lite/schema/schema_generated.h"
+#include "tensorflow/lite/version.h"
+#include "tf_hallo_world_model.h"
+
+// This constant represents the range of x values our model was trained on,
+// which is from 0 to (2 * Pi). We approximate Pi to avoid requiring additional
+// libraries.
+const float kXrange = 2.f * 3.14159265359f;
 
 // This constant determines the number of inferences to perform across the range
 // of x values defined above. Since each inference takes time, the higher this
@@ -7,19 +19,6 @@
 // of time. Since different devices take different amounts of time to perform
 // inference, this value should be defined per-device.
 const int kInferencesPerCycle = 20;
-
-// This constant represents the range of x values our model was trained on,
-// which is from 0 to (2 * Pi). We approximate Pi to avoid requiring additional
-// libraries.
-const float kXrange = 2.f * 3.14159265359f;
-
-#include "tensorflow/lite/micro/all_ops_resolver.h"
-#include "tensorflow/lite/micro/micro_error_reporter.h"
-#include "tensorflow/lite/micro/micro_interpreter.h"
-#include "tensorflow/lite/micro/testing/micro_test.h"
-#include "tensorflow/lite/schema/schema_generated.h"
-#include "tensorflow/lite/version.h"
-#include "tf_hallo_world_model.h"
 
 // Globals, used for compatibility with Arduino-style sketches.
 namespace {
